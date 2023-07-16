c3d9ab77937749787d964e78b5073fdad362419c
Hongzhou Luan <66193810+hzluan@users.noreply.github.com>
2023-07-16 17:23:55 +0800

    commit

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd31fd3132a546d70db2a228c21ce736.js"
+  "/precache-manifest.3609337be6d4a8874aaa7df7b95959a6.js"
 );
 
 self.addEventListener('message', (event) => {
