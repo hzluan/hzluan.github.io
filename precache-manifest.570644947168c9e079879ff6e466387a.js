7885724294540b2fb5cd142e8bb3b26ccc861f25
Hongzhou Luan <66193810+hzluan@users.noreply.github.com>
2023-07-16 17:34:32 +0800

    commit

---
+++ precache-manifest.570644947168c9e079879ff6e466387a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63dea9a78cc49284f78804b8abb871f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a791450fe35f62af7b15",
+    "url": "/static/css/2.8bfa8659.chunk.css"
+  },
+  {
+    "revision": "78d641d2bd2b909e401f",
+    "url": "/static/css/main.3f09121e.chunk.css"
+  },
+  {
+    "revision": "a791450fe35f62af7b15",
+    "url": "/static/js/2.ae2b3713.chunk.js"
+  },
+  {
+    "revision": "52323fc63d3a9ebc46842e47b791bdc2",
+    "url": "/static/js/2.ae2b3713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78d641d2bd2b909e401f",
+    "url": "/static/js/main.cd15c5d2.chunk.js"
+  },
+  {
+    "revision": "f4800b06d7eae560929f",
+    "url": "/static/js/runtime-main.c72e126b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b891949ec4d3d70939979b0b5c3e8253.js"
+  "/precache-manifest.570644947168c9e079879ff6e466387a.js"
 );
 
 self.addEventListener('message', (event) => {
